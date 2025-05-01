ff89bc49cc429f7b6f5a3a9db0e7f25bdc0a0db2
Jason Son <jasonson2004@gmail.com>
2025-05-01 14:53:33 -0700

    add: vercel component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -30,6 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
       </Head>
+      <Analytics />
       <Component {...pageProps} />
     </div>
   );
